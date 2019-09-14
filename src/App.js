216c369d2f6bd127f5197fbf028bf05e77239c10
sriramteja04 <s531524@nwmissouri.edu>
2019-09-14 01:35:02 -0500

    Written first test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from './components/header';
 
 function App() {
-  return <div className='App'>
-    
-  </div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
