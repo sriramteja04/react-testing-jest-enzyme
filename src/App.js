b6363de93638742c3e5d07b90fc5f287159636cf
sriramteja04 <s531524@nwmissouri.edu>
2019-09-14 21:27:50 -0500

    added new testing file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/header';
+import Headline from './components/headline';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <section className='main'>
+        <Headline header='Posts' desc='click the button to render posts' />
+      </section>
     </div>
   );
 }
