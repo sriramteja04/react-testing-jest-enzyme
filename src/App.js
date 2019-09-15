b5b8d3220e31be2389b336e6a8a3f9483579d0ac
sriramteja04 <s531524@nwmissouri.edu>
2019-09-14 23:17:55 -0500

    Added the prop-types in Headline component and tested prop-types

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.scss';
 import Header from './components/header';
 import Headline from './components/headline';
 
+const tempArr = [
+  {
+    fName: 'Sri Ram Teja',
+    lName: 'Komerisetti',
+    email: 'sriramteja04@gmail.com',
+    age: 24,
+    onlineStatus: true
+  }
+];
+
 function App() {
   return (
     <div className='App'>
       <Header />
       <section className='main'>
-        <Headline header='Posts' desc='click the button to render posts' />
+        <Headline header='Posts' desc='click the button to render posts' tempArr={tempArr} />
       </section>
     </div>
   );
